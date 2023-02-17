5df5905000ff230c9448b02a9c52717334014487
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-02-17 15:04:23 +0100

    done

--- js/main.js
+++ js/main.js
@@ -4,9 +4,21 @@ const app = createApp({
   data() {
     return {
       message: "how are you?",
+      indirizziEmail: [],
     };
   },
 
-  methods: {},
+  methods: {
+    stampaEmail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get(" https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.indirizziEmail.push(response.data.response);
+            console.log(response);
+          });
+      }
+    },
+  },
 });
 app.mount("#root");
